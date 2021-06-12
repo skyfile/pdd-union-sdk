b4fd5d8624b2d56c0d2884b32054f355eb19117b
吴伟臻 <>
2021-06-12 09:43:53 +0800

    update: 添加多多礼金状态更新接口

---
+++ src/Api/Request/PddDdkCashGiftStatusUpdateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Com\Pdd\Pop\Sdk\Api\Request;
+
+class PddDdkCashGiftStatusUpdateRequest extends PopBaseHttpRequest
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @JsonProperty(Long "cash_gift_id")
+     */
+    private $cashGiftId;
+
+    /**
+     * @JsonProperty(Integer "update_type")
+     */
+    private $updateType;
+
+    protected function setUserParams(&$params)
+    {
+        $this->setUserParam($params, "cash_gift_id", $this->cashGiftId);
+        $this->setUserParam($params, "update_type", $this->updateType);
+    }
+
+    public function getVersion()
+    {
+        return "V1";
+    }
+
+    public function getDataType()
+    {
+        return "JSON";
+    }
+
+    public function getType()
+    {
+        return "pdd.ddk.cashgift.status.update";
+    }
+
+    public function setCashGiftId($cashGiftId)
+    {
+        $this->cashGiftId = $cashGiftId;
+    }
+
+    public function setUpdateType($type)
+    {
+        $this->updateType = $type;
+    }
+}
