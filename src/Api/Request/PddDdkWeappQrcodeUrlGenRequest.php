2f5ff7432ab4f9f9d140e853712b57934d7214f6
吴伟臻 <>
2021-08-03 15:10:33 +0800

    update: 修改PDD小程序码生成接口

--- src/Api/Request/PddDdkWeappQrcodeUrlGenRequest.php
+++ src/Api/Request/PddDdkWeappQrcodeUrlGenRequest.php
@@ -7,82 +7,93 @@ use Com\Pdd\Pop\Sdk\PopBaseJsonEntity;
 class PddDdkWeappQrcodeUrlGenRequest extends PopBaseHttpRequest
 {
     public function __construct()
-	{
+    {
 
-	}
-	/**
-	* @JsonProperty(String, "custom_parameters")
-	*/
-	private $customParameters;
+    }
+    /*
+     * @jsonProperty(String, "cash_gift_id")
+     */
+    private $cashGiftId;
 
-	/**
-	* @JsonProperty(Boolean, "generate_mall_collect_coupon")
-	*/
-	private $generateMallCollectCoupon;
+    /**
+     * @JsonProperty(String, "custom_parameters")
+     */
+    private $customParameters;
 
-	/**
-	* @JsonProperty(List<Long>, "goods_id_list")
-	*/
-	private $goodsIdList;
+    /**
+     * @JsonProperty(Boolean, "generate_mall_collect_coupon")
+     */
+    private $generateMallCollectCoupon;
 
-	/**
-	* @JsonProperty(String, "p_id")
-	*/
-	private $pId;
+    /**
+     * @JsonProperty(List<Long>, "goods_sign_list")
+     */
+    private $goodsSignList;
 
-	/**
-	* @JsonProperty(Long, "zs_duo_id")
-	*/
-	private $zsDuoId;
+    /**
+     * @JsonProperty(String, "p_id")
+     */
+    private $pId;
 
-	protected function setUserParams(&$params)
-	{
-		$this->setUserParam($params, "custom_parameters", $this->customParameters);
-		$this->setUserParam($params, "generate_mall_collect_coupon", $this->generateMallCollectCoupon);
-		$this->setUserParam($params, "goods_id_list", $this->goodsIdList);
-		$this->setUserParam($params, "p_id", $this->pId);
-		$this->setUserParam($params, "zs_duo_id", $this->zsDuoId);
+    /**
+     * @JsonProperty(Long, "zs_duo_id")
+     */
+    private $zsDuoId;
 
-	}
+    protected function setUserParams(&$params)
+    {
+        $this->setUserParam($params, "cash_gift_id", $this->cashGiftId);
+        $this->setUserParam($params, "custom_parameters", $this->customParameters);
+        $this->setUserParam($params, "generate_mall_collect_coupon", $this->generateMallCollectCoupon);
+        $this->setUserParam($params, "goods_sign_list", $this->goodsSignList);
+        $this->setUserParam($params, "p_id", $this->pId);
+        $this->setUserParam($params, "zs_duo_id", $this->zsDuoId);
 
-	public function getVersion()
-	{
-		return "V1";
-	}
+    }
 
-	public function getDataType()
-	{
-		return "JSON";
-	}
+    public function getVersion()
+    {
+        return "V1";
+    }
 
-	public function getType()
-	{
-		return "pdd.ddk.weapp.qrcode.url.gen";
-	}
+    public function getDataType()
+    {
+        return "JSON";
+    }
 
-	public function setCustomParameters($customParameters)
-	{
-		$this->customParameters = $customParameters;
-	}
+    public function getType()
+    {
+        return "pdd.ddk.weapp.qrcode.url.gen";
+    }
 
-	public function setGenerateMallCollectCoupon($generateMallCollectCoupon)
-	{
-		$this->generateMallCollectCoupon = $generateMallCollectCoupon;
-	}
+    public function setCashGiftId($cashGiftId)
+    {
+        $this->cashGiftId = $cashGiftId;
+    }
 
-	public function setGoodsIdList($goodsIdList)
-	{
-		$this->goodsIdList = $goodsIdList;
-	}
+    public function setCustomParameters($customParameters)
+    {
+        $this->customParameters = $customParameters;
+    }
 
-	public function setPId($pId)
-	{
-		$this->pId = $pId;
-	}
+    public function setGenerateMallCollectCoupon($generateMallCollectCoupon)
+    {
+        $this->generateMallCollectCoupon = $generateMallCollectCoupon;
+    }
 
-	public function setZsDuoId($zsDuoId)
-	{
-		$this->zsDuoId = $zsDuoId;
-	}
+    public function setGoodsSignList($goodsSignList)
+    {
+        $this->goodsSignList = $goodsSignList;
+    }
+
+    public function setPId($pId)
+    {
+        $this->pId = $pId;
+    }
+
+    public function setZsDuoId($zsDuoId)
+    {
+        $this->zsDuoId = $zsDuoId;
+    }
 
 }
